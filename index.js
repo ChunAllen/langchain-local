847f1579918c73a7da14ec2ec730dc12731b68b1
Allen Chun <allen_oliver_marcos_chun_from.scientec@stb.gov.sg>
2023-07-09 13:45:52 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+// Documentation: https://betterprogramming.pub/build-a-chatbot-on-your-csv-data-with-langchain-and-openai-ed121f85f0cd
+
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { JSONLoader } from "langchain/document_loaders/fs/json";
+
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain, loadQARefineChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+//3. Import env variables
+import * as dotenv from 'dotenv';
+
+//4. Import others
+import fs from 'fs';
+
+// Load Environment Variables
+dotenv.config()
+
+const loader = new DirectoryLoader("./docs", {
+  ".json": (path) => new JSONLoader(path),
+  ".txt": (path) => new TextLoader(path)
+})
+const VECTOR_STORE_PATH = "Documents.index";
+
+const normalizeDocuments = (docs) => {
+  return docs.map((doc) => {
+    if (typeof doc.pageContent === "string") {
+      return doc.pageContent;
+    } else if (Array.isArray(doc.pageContent)) {
+      return doc.pageContent.join("\n");
+    }
+  });
+}
+
+// Main Function
+export const run = async (params) => {
+  const prompt = params[0]
+  console.log('Prompt:', prompt)
+
+  console.log("Loading docs...")
+  const docs = await loader.load();
+
+  console.log('Processing...')
+  const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY });
+
+  let vectorStore;
+
+  // if (fs.existsSync(VECTOR_STORE_PATH)) {
+  //   console.log('Loading existing vector store...')
+  //   vectorStore = await HNSWLib.load(
+  //     VECTOR_STORE_PATH,
+  //     new OpenAIEmbeddings()
+  //   );
+  //   console.log("Vector store loaded.");
+  // } else {
+  console.log('Creating new vector store...')
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+  });
+  const normalizedDocs = normalizeDocuments(docs);
+  const splitDocs = await textSplitter.createDocuments(normalizedDocs);
+
+  vectorStore = await HNSWLib.fromDocuments(
+    splitDocs,
+    new OpenAIEmbeddings()
+  );
+
+  await vectorStore.save(VECTOR_STORE_PATH);
+  console.log("Vector store created.")
+  // }
+
+  console.log("Creating retrieval chain...")
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever())
+
+  // const chain = new RetrievalQAChain({
+  //   combineDocumentsChain: loadQARefineChain(model),
+  //   retriever: vectorStore.asRetriever(),
+  // });
+
+  console.log("Querying chain...")
+  const res = await chain.call({ query: prompt })
+
+  // const res = await model.call(prompt)
+
+  console.log(res)
+
+  // console.log({ res })
+}
+
+run(process.argv.slice(2))
